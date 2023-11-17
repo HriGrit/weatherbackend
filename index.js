744d622a0917d29f3c318e6cfc86af48f06175ce
HriGrit <86262322+HriGrit@users.noreply.github.com>
2023-11-17 12:07:57 +0530

    init

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const axios = require("axios");
 const cors = require("cors");
 
+require("dotenv").config();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
